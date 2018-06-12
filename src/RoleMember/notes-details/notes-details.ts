af293ece86a7e101ed4144feafec181e13154c9f
Abhin Pai <abhinpai96@gmail.com>
2018-06-12 15:20:27 +0530

    Edit, Add, View notes and UI added

--- src/RoleMember/notes-details/notes-details.ts
+++ src/RoleMember/notes-details/notes-details.ts
@@ -1,5 +1,6 @@
+
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,72 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NotesDetailsPage {
 
-  item = [1,2,3];
+  item = [1, 2, 3];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public actionCtrl: ActionSheetController,
+    public alertCtrl: AlertController,
+    public navParams: NavParams) {
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionCtrl.create({
+      title: 'Manipulate Notes Data ',
+      buttons: [
+        {
+          text: 'Edit',
+          handler: () => {
+            this.goToNotesEditPage();
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.deleteNote();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+  goToNotesEditPage() {
+    this.navCtrl.push("EditNotePage");
+  }
+
+  goToAddNotePage(){
+    this.navCtrl.push("AddNotePage");
+  }
+
+  goToViewNotePage() {
+    this.navCtrl.push("ViewNotePage");
+  }
+
+  deleteNote() {
+    const confirm = this.alertCtrl.create({
+      title: 'Delete note',
+      message: 'Are you sure you want to delete?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Yes, Delete',
+          handler: () => {
+
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 
